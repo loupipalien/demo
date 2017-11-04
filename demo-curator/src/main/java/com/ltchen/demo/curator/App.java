a4fdcf671cf75063e4335a8ebba4ad1833aab28d
ltchen <loupipalien@gmail.com>
2017-11-04 23:26:34 +0800

    curator-demo

--- demo-curator/src/main/java/com/ltchen/demo/curator/App.java
+++ demo-curator/src/main/java/com/ltchen/demo/curator/App.java
@@ -1,17 +1,29 @@
 package com.ltchen.demo.curator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 
-    private static String zkConnStr = "192.168.0.126:2181";
+	private static String zkConnStr = "192.168.0.126:2181";
 
-    public static void main(String[] args) throws Exception {
-       ZKCliHelper zkCliHelper = new ZKCliHelper();
-       zkCliHelper.setZkConnStr(zkConnStr);
-       zkCliHelper.init();
-       zkCliHelper.createPath("/ltchen", "wawo");
-//       System.out.println(zkCliHelper.getData("/ltchen"));
-       zkCliHelper.destory();
-    }
+	public static void main(String[] args) throws Exception {
+		// ZKCliHelper zkCliHelper = new ZKCliHelper();
+		// zkCliHelper.setZkConnStr(zkConnStr);
+		// zkCliHelper.init();
+		// zkCliHelper.createPath("/ltchen", "wawo");
+		//// System.out.println(zkCliHelper.getData("/ltchen"));
+		// zkCliHelper.destory();
 
-  
+		List<String> list = new ArrayList<String>();
+		list.add("1");
+		list.add("2");
+		for (String item : list) {
+			if ("2".equals(item)) {
+				list.remove(item);
+			}
+		}
+		System.out.println(list);
+	}
+
 }
--- demo-dubbo-consumer/src/main/java/com/ltchen/demo/dubbo/consumer/App.java
+++ demo-dubbo-consumer/src/main/java/com/ltchen/demo/dubbo/consumer/App.java
@@ -3,14 +3,37 @@ package com.ltchen.demo.dubbo.consumer;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.NDC;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * Hello world!
- *
+ * 
+ * @file : App.java
+ * @date : 2017年10月22日
+ * @author : ltchen
+ * @email : loupipalien@gmail.com
+ * @desc :
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+	
+	private static Logger LOG = LoggerFactory.getLogger(App.class);
+	
+    public static void main( String[] args ) {
+    	LOG.info("Hello World!");
         System.out.println( "Hello World!" );
+        LOG.info("Make sure %x is in your layout pattern!");
+        // 从客户端获得IP地址的例子
+        String[] ips = {"192.168.0.10","192.168.0.27"};
+        // 模拟一个运行方法
+        for (int i = 0; i<ips.length ; i++)  
+        {
+         // 将IP放进 NDC中
+         NDC.push(ips[i]);
+         LOG.info("A NEW client connected, who's ip should appear in this log message.");
+         NDC.pop();
+        }
+        NDC.remove();
+        LOG.info("Finished.");
     }
 }
